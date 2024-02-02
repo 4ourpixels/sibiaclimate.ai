265d007d4c1ed6380865540f2aef15eed0697b43
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-02-02 22:19:32 +0300

    First commit 🚀

--- website/views.py
+++ website/views.py
@@ -6,18 +6,16 @@ from django.contrib.auth.decorators import login_required
 
 
 def blog_list(request):
-    title_tag = "Blogs"
     context = {
-        'title_tag': title_tag,
+        'title_tag': "Blogs",
         'blogs': BlogPost.objects.all(),
     }
     return render(request, 'blog/blog-list.html', context)
 
 
 def index(request):
-    title_tag = "Home"
     context = {
-        'title_tag': title_tag,
+        'title_tag': "Home",
     }
     return render(request, 'index.html', context)
 
@@ -43,13 +41,12 @@ def blog_detail(request, slug):
 
 def edit_blog(request, slug):
     blog = get_object_or_404(BlogPost, slug=slug)
-    title_tag = f"Update: {blog.title}"
     if request.method == 'POST':
         edit_blog_form = BlogForm(request.POST, request.FILES, instance=blog)
         if edit_blog_form.is_valid():
             edit_blog_form.save()
             messages.info(request, f'"{blog.title}" update was successful!')
-            return redirect('blog_detail', tag_slug, slug)
+            return redirect('blog_detail', slug)
         else:
             print("Errors occurred while uploading: ",
                   edit_blog_form.errors)
@@ -58,7 +55,7 @@ def edit_blog(request, slug):
 
     context = {
         'edit_blog_form': edit_blog_form,
-        'title_tag': title_tag,
+        'title_tag': f"Update: {blog.title}",
         'blog': blog,
     }
 
@@ -67,8 +64,6 @@ def edit_blog(request, slug):
 
 @login_required(login_url='login')
 def add_blog(request):
-    title_tag = "Add new blog"
-
     if request.method == 'POST':
         new_blog_form = BlogForm(request.POST, request.FILES)
         if new_blog_form.is_valid():
@@ -76,12 +71,12 @@ def add_blog(request):
             new_blog.author = request.user
             new_blog.save()
             messages.info(request, f'"{new_blog.title}" was added!')
-            return redirect('blog_detail', new_blog.tag.slug, new_blog.slug)
+            return redirect('blog_detail', new_blog.slug)
     else:
         new_blog_form = BlogForm()
 
     context = {
-        'title_tag': title_tag,
+        'title_tag': "Add new blog",
         'new_blog_form': new_blog_form
     }
 
