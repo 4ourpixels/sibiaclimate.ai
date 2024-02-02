c2bfd4591c404d890189e29c25669097d28b0d94
Four Pixels <121856163+4ourpixels@users.noreply.github.com>
2024-02-02 22:17:30 +0300

    First commit 🚀

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from .views import index, blog_detail, blog_list, edit_blog, add_blog
 urlpatterns = [
     path('', index, name='index'),
     path('blogs', blog_list, name='blog_list'),
-    path('<slug:tag_slug>/<slug:slug>/', blog_detail, name='blog_detail'),
-    path('edit-blog/<slug:tag_slug>/<slug:slug>/', edit_blog, name='edit_blog'),
+    path('blog/<slug:slug>/', blog_detail, name='blog_detail'),
+    path('edit-blog/<slug:slug>/', edit_blog, name='edit_blog'),
     path('add-blog/', add_blog, name='add_blog'),
 ]
